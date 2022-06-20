82e58b3fd3a353fc68651e141a96cedb27cf919f
jesusj <jajm84@gmail.com>
2022-06-19 22:36:11 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// Algoritmo para calcular el promedio de 4 notas de un estudiante. Si el promedio es mayor o igual a 10 se muestra "Aprobado" y si el promedio es menor a 10 muestra "Reprobado"
+
+let notaUno = parseFloat(prompt("Ingrese nota 1er parcial"));
+let notaDos = parseFloat(prompt("Ingrese nota 2do parcial"));
+let notaTres = parseFloat(prompt("Ingrese nota 3er parcial"));
+let notaCuatro = parseFloat(prompt("Ingrese nota 4tp parcial"));
+let promedio = (notaUno+notaDos+notaTres+notaCuatro)/4;
+console.log(promedio);
+
+if (promedio >= 10) {
+    alert("Esta Aprobado")
+    
+} else {
+
+    alert("Esta Reprobado")
+    
+}
